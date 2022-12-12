b338aac89d261aa0bd617987c30f320cdab40d51
slicina7 <slicina1@etf.unsa.ba>
2022-12-12 18:47:55 +0100

    Added comment

--- src/main/java/ba/unsa/etf/rpr/dao/BrandDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/BrandDao.java
@@ -2,5 +2,11 @@ package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Brand;
 
+/**
+ * Dao interface for Brand domain bean
+ *
+ * @author Selma Licina
+ */
+
 public interface BrandDao extends Dao<Brand>{
 }
