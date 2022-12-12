baef872d93f134000868b82a1dbc47cf4ad733e1
slicina7 <slicina1@etf.unsa.ba>
2022-12-12 17:25:55 +0100

    Added class Purchase

---
+++ src/main/java/ba/unsa/etf/rpr/domain/Purchase.java
@@ -0,0 +1,54 @@
+package ba.unsa.etf.rpr.domain;
+
+import java.util.Objects;
+
+public class Purchase {
+    private int id;
+    private Buyer buyer;
+    private Phone phone;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Buyer getBuyer() {
+        return buyer;
+    }
+
+    public void setBuyer(Buyer buyer) {
+        this.buyer = buyer;
+    }
+
+    public Phone getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Phone phone) {
+        this.phone = phone;
+    }
+    @Override
+    public String toString(){
+        return "Phone{" +
+                "id=" + id +
+                ", buyer=" +  buyer +
+                ", phone=" +  phone +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Purchase purchase = (Purchase) o;
+        return id == purchase.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, buyer, phone);
+    }
+}
