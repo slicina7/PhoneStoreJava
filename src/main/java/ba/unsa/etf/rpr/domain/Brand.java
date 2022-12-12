05958036810ec28a168b8701563e96ab99c73122
slicina7 <slicina1@etf.unsa.ba>
2022-12-12 16:57:05 +0100

    Added class Brand

---
+++ src/main/java/ba/unsa/etf/rpr/domain/Brand.java
@@ -0,0 +1,42 @@
+package ba.unsa.etf.rpr.domain;
+
+import java.util.Objects;
+
+public class Brand {
+    private int id;
+    private String name;
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id=id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString(){
+        return "Brand" +
+                "id=" + id +
+                ", name=" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if(this==o) return true;
+        if(o==null || getClass()!=o.getClass()) return false;
+        Brand brand=(Brand) o;
+        return id==brand.id;
+    }
+    @Override
+    public int hashCode(){
+        return Objects.hash(id,name);
+    }
+}
