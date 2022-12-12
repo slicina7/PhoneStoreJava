f60208700af0b67220c4b8ab29354bdf6247d801
slicina7 <slicina1@etf.unsa.ba>
2022-12-12 20:52:34 +0100

    Added methods and comments

--- src/main/java/ba/unsa/etf/rpr/dao/PurchaseDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/PurchaseDao.java
@@ -1,6 +1,29 @@
 package ba.unsa.etf.rpr.dao;
 
+import ba.unsa.etf.rpr.domain.Buyer;
+import ba.unsa.etf.rpr.domain.Phone;
 import ba.unsa.etf.rpr.domain.Purchase;
 
+import java.util.List;
+
+/**
+ * Dao interface for Purchase domain bean
+ *
+ * @author Selma Licina
+ */
 public interface PurchaseDao extends Dao<Purchase> {
+    /**
+     * Returns all purchases  of that phone.
+     *
+     * @param phone search string for purchases
+     * @return list of purchases
+     */
+    List<Purchase> searchByPhone(Phone phone);
+    /**
+     * Returns all purchases of that buyer.
+     *
+     * @param buyer search string for purchases
+     * @return list of purchases
+     */
+    List<Purchase> searchByBuyer(Buyer buyer);
 }
