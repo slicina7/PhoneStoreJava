20d4315a24f5c0448e06d7a0baa916defce46830
slicina7 <slicina1@etf.unsa.ba>
2022-12-12 18:53:49 +0100

    Added method

--- src/main/java/ba/unsa/etf/rpr/dao/PhoneDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/PhoneDao.java
@@ -1,6 +1,31 @@
 package ba.unsa.etf.rpr.dao;
 
+import ba.unsa.etf.rpr.domain.Brand;
 import ba.unsa.etf.rpr.domain.Phone;
 
+import java.util.List;
+
+/**
+ * Dao interface for Phone domain bean
+ *
+ * @author Selma Licina
+ */
+
 public interface PhoneDao extends Dao<Phone>{
+
+    /**
+     * Returns all phones that contains given text.
+     *
+     * @param text search string for phones
+     * @return list of phones
+     */
+    List<Phone> searchByText(String text);
+
+    /**
+     * Returns all phones that are given brand.
+     *
+     * @param brand search string for phones
+     * @return list of phones
+     */
+    List<Phone> searchByBrand(Brand brand);
 }
