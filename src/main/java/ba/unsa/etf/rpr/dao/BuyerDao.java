ce7d0aff757a522117636ad594fdf641a6cb0d03
slicina7 <slicina1@etf.unsa.ba>
2022-12-12 20:48:49 +0100

    Added comment

--- src/main/java/ba/unsa/etf/rpr/dao/BuyerDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/BuyerDao.java
@@ -2,5 +2,11 @@ package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Buyer;
 
+/**
+ * Dao interface for Buyer domain bean
+ *
+ * @author Selma Licina
+ */
+
 public interface BuyerDao extends Dao<Buyer>{
 }
