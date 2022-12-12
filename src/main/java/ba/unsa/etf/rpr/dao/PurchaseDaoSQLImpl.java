61d9ec80a705040c7882544402b2ceb56fcebdab
slicina7 <slicina1@etf.unsa.ba>
2022-12-12 23:22:10 +0100

    Added method getById

---
+++ src/main/java/ba/unsa/etf/rpr/dao/PurchaseDaoSQLImpl.java
@@ -0,0 +1,72 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.domain.Buyer;
+import ba.unsa.etf.rpr.domain.Phone;
+import ba.unsa.etf.rpr.domain.Purchase;
+
+import java.sql.*;
+import java.util.List;
+
+public class PurchaseDaoSQLImpl implements PurchaseDao {
+
+    private Connection connection;
+
+    public PurchaseDaoSQLImpl(){
+        try{
+            this.connection= DriverManager.getConnection("url","user","password");
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    @Override
+    public Purchase getById(int id) {
+        String query = "SELECT * FROM purchases WHERE id = ?";
+        try{
+            PreparedStatement stmt=this.connection.prepareStatement(query);
+            stmt.setInt(1,id);
+            ResultSet rs=stmt.executeQuery();  // rs is iterator
+            if(rs.next()){
+                Purchase purchase=new Purchase();
+                purchase.setId(rs.getInt("id"));
+                purchase.setBuyer(new BuyerDaoSQLImpl().getById(rs.getInt(1)));
+                purchase.setPhone(new PhoneDaoSQLImpl().getById(rs.getInt(1)));
+                rs.close();
+                return purchase;
+            }else
+                return null; // no elements in rs
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public Purchase insert(Purchase item) {
+        return null;
+    }
+
+    @Override
+    public Purchase update(Purchase item) {
+        return null;
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+
+    @Override
+    public List<Purchase> getAll() {
+        return null;
+    }
+
+    @Override
+    public List<Purchase> searchByPhone(Phone phone) {
+        return null;
+    }
+
+    @Override
+    public List<Purchase> searchByBuyer(Buyer buyer) {
+        return null;
+    }
+}
