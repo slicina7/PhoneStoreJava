75b6443e08e018046e58fec7ec00a9068d1b9ba8
slicina7 <slicina1@etf.unsa.ba>
2022-12-13 23:34:43 +0100

    Added tableView method

--- src/main/java/ba/unsa/etf/rpr/dao/PhoneDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/PhoneDaoSQLImpl.java
@@ -1,7 +1,6 @@
 package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Brand;
-import ba.unsa.etf.rpr.domain.Buyer;
 import ba.unsa.etf.rpr.domain.Phone;
 
 import java.io.FileReader;
@@ -69,15 +68,15 @@ public class PhoneDaoSQLImpl implements PhoneDao{
 
     @Override
     public Phone update(Phone item) {
-        String insert = "UPDATE phones SET version=? WHERE id = ?";
+        String insert = "UPDATE phones SET brand_id=?,version=?,price=?,in_stock=?,release_date=? WHERE id = ?";
         try{
             PreparedStatement stmt = this.connection.prepareStatement(insert);
-            stmt.setInt(1, item.getId());
-            stmt.setInt(2, item.getBrand().getId());
-            stmt.setString(3,item.getVersion());
-            stmt.setInt(4,item.getPrice());
-            stmt.setInt(5,item.getIn_stock());
-            stmt.setDate(6, (Date) item.getRelease_date());
+            stmt.setInt(1, item.getBrand().getId());
+            stmt.setString(2,item.getVersion());
+            stmt.setInt(3,item.getPrice());
+            stmt.setInt(4,item.getIn_stock());
+            stmt.setDate(5, (Date) item.getRelease_date());
+            stmt.setInt(6, item.getId());
             stmt.executeUpdate();
             return item;
         }catch (SQLException e){
@@ -117,13 +116,51 @@ public class PhoneDaoSQLImpl implements PhoneDao{
             }
             rs.close();
         }catch (SQLException e){
-            e.printStackTrace(); // poor error handling
+            e.printStackTrace();
         }
         return phones;
     }
 
     @Override
     public List<Phone> searchByBrand(Brand brand) {
-        return null;
+        String query="SELECT * FROM phones WHERE brand_id=?";
+        List<Phone> phones = new ArrayList<Phone>();
+        try{
+            PreparedStatement stmt = this.connection.prepareStatement(query);
+            stmt.setInt(1,brand.getId());
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()){ // result set is iterator.
+                Phone phone=new Phone();
+                phone.setId(rs.getInt("id"));
+                phone.setBrand(new BrandDaoSQLImpl().getById(rs.getInt(2)));
+                phone.setVersion(rs.getString("version"));
+                phone.setPrice(rs.getInt("price"));
+                phone.setIn_stock(rs.getInt("in_stock"));
+                phone.setRelease_date(rs.getDate("release_date"));
+                phones.add(phone);
+            }
+            rs.close();
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return phones;
+    }
+    public void tableView() {
+        String query="SELECT * FROM phones";
+        try {
+            PreparedStatement stmt = this.connection.prepareStatement(query);
+            ResultSet rs = stmt.executeQuery();
+            while(rs.next()){
+                int id=rs.getInt("id");
+                int brand_id=rs.getInt("brand_id");
+                String version=rs.getString("version");
+                int price=rs.getInt("price");
+                int in_stock=rs.getInt("in_stock");
+                Date release_date=rs.getDate("release_date");
+                System.out.println(id+" "+brand_id+" "+version+" "+price+" "+in_stock+" "+release_date);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
