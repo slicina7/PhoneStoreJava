cea3be30581d9d71eb9b0917470492a720982fbb
slicina7 <slicina1@etf.unsa.ba>
2022-12-19 00:31:28 +0100

    First version

---
+++ src/main/java/ba/unsa/etf/rpr/MainFX.java
@@ -0,0 +1,25 @@
+package ba.unsa.etf.rpr;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class MainFX extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/login.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root, 600, 500);
+        stage.setResizable(false);
+        stage.setTitle("Login");
+        //stage.getIcons().add(new Image(""));
+        stage.setScene(scene);
+        stage.show();
+    }
+    public static void main (String[] args) {
+        launch();
+    }
+}
