dc186bba3af85584d11cad0cb78180877b3d334e
slicina7 <slicina1@etf.unsa.ba>
2022-12-19 00:31:08 +0100

    First version

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/MainController.java
@@ -0,0 +1,21 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class MainController {
+    public TextField idEmail;
+    public PasswordField idPassword;
+    public Button idLogin;
+
+    public void akcijaEmail(ActionEvent actionEvent) {
+    }
+
+    public void akcijaPassworda(ActionEvent actionEvent) {
+    }
+
+    public void akcijaLoginDugme(ActionEvent actionEvent) {
+    }
+}
