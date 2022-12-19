f1c8b4b65b238ffbb095d21cfae7cc7c35ee6359
slicina7 <slicina1@etf.unsa.ba>
2022-12-19 17:13:10 +0100

    Connecting Sign up conttroller to log in

--- src/main/java/ba/unsa/etf/rpr/controllers/SignupController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/SignupController.java
@@ -1,9 +1,17 @@
 package ba.unsa.etf.rpr.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 public class SignupController {
 
@@ -17,6 +25,20 @@ public class SignupController {
 
     public void signupButtonAction(ActionEvent actionEvent) {
     }
-    public void loginButtonAction(ActionEvent actionEvent) {
+    public void loginButtonAction(ActionEvent actionEvent) throws IOException{
+        try {
+            Stage stage = new Stage();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/login.fxml"));
+            Parent root = loader.load();
+            Scene scene = new Scene(root,USE_COMPUTED_SIZE,USE_COMPUTED_SIZE);
+            stage.setTitle("Sing up");
+            stage.setScene(scene);
+            stage.setResizable(false);
+            stage.show();
+            Stage primaryStage = (Stage) idSignup.getScene().getWindow();
+            primaryStage.hide();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
