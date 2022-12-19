ea35a9e8120e05051ed1572c688d78511c353482
slicina7 <slicina1@etf.unsa.ba>
2022-12-19 17:06:22 +0100

    Adding methods and atributes

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/SignupController.java
@@ -0,0 +1,22 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class SignupController {
+
+    public TextField idName;
+    public TextField idSurname;
+    public TextField idEmail;
+    public TextField idAccountNumber;
+    public PasswordField idPassword;
+    public Button idSignup;
+    public Button idLogin;
+
+    public void signupButtonAction(ActionEvent actionEvent) {
+    }
+    public void loginButtonAction(ActionEvent actionEvent) {
+    }
+}
