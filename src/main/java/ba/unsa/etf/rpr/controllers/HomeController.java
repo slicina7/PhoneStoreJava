368e02d4212c6af004b1e5c475876badb708bb0b
slicina7 <slicina1@etf.unsa.ba>
2022-12-26 21:16:44 +0100

    Added listener for listview

--- src/main/java/ba/unsa/etf/rpr/controllers/HomeController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/HomeController.java
@@ -3,26 +3,56 @@ package ba.unsa.etf.rpr.controllers;
 import ba.unsa.etf.rpr.dao.BrandDaoSQLImpl;
 import ba.unsa.etf.rpr.dao.PhoneDaoSQLImpl;
 import ba.unsa.etf.rpr.domain.Brand;
+import ba.unsa.etf.rpr.domain.Buyer;
 import ba.unsa.etf.rpr.domain.Phone;
-
+import ba.unsa.etf.rpr.exception.BuyerException;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class HomeController {
+    public ListView<Brand> brandsListView;
     private BrandDaoSQLImpl brandDaoSQL;
-    private PhoneDaoSQLImpl phoneDaoSQL;
     private ObservableList<Brand> brands;
+    private PhoneDaoSQLImpl phoneDaoSQL;
     private ObservableList<Phone> phones;
     public TableView<Phone> phonesTableView;
-    public ListView<Brand> brandsList;
+    public TableColumn<Phone,String> colPhonesId;
+    public TableColumn<Phone,String> colPhonesVersion;
+    public TableColumn<Phone,String> colPhonesDate;
+    public TableColumn<Phone,String> colPhonesStock;
+    public TableColumn<Phone,String> colPhonesPrice;
+
 
     public HomeController(){
-        phoneDaoSQL=new PhoneDaoSQLImpl();
         brandDaoSQL=new BrandDaoSQLImpl();
-        brands= FXCollections.observableArrayList(brandDaoSQL.getAll());
+        phoneDaoSQL=new PhoneDaoSQLImpl();
+        brands=FXCollections.observableArrayList(brandDaoSQL.getAll());
         phones=FXCollections.observableArrayList();
+    }
+    @FXML
+    public void initialize(){
+        brandsListView.setItems(brands);
+        colPhonesId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        colPhonesVersion.setCellValueFactory(new PropertyValueFactory<>("version"));
+        colPhonesDate.setCellValueFactory(new PropertyValueFactory<>("release_date"));
+        colPhonesStock.setCellValueFactory(new PropertyValueFactory<>("in_stock"));
+        colPhonesPrice.setCellValueFactory(new PropertyValueFactory<>("price"));
+        brandsListView.getSelectionModel().selectedItemProperty().addListener((obs, oldCat, newCat) -> {
+            try {
+                phonesTableView.getItems().clear();
+                phones.addAll(phoneDaoSQL.searchByBrand(newCat));
+                phonesTableView.refresh();
+                phonesTableView.setItems(phones);
+            } catch (BuyerException e) {
+                System.out.println("Something went wrong with searchByCategory method from quoteDaoSQlImpl");
+                throw new RuntimeException(e);
+            }
+        });
     }
 }
 
