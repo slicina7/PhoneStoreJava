0bd4a7b4b29d7f8877984ae6f4ff9e563fd1b968
slicina7 <slicina1@etf.unsa.ba>
2022-12-29 21:04:09 +0100

    Added regex validation to listeners

--- src/main/java/ba/unsa/etf/rpr/controllers/SignupController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/SignupController.java
@@ -1,5 +1,6 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.dao.BuyerDaoSQLImpl;
 import ba.unsa.etf.rpr.domain.Buyer;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
@@ -8,11 +9,11 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 import java.util.Random;
 
@@ -38,7 +39,8 @@ public class SignupController {
         idName.textProperty().addListener(new ChangeListener<String>() {
             @Override
             public void changed(ObservableValue<? extends String> observableValue, String o, String n) {
-                if (idName.getText().trim().isEmpty()) {
+
+                if (idName.getText().trim().isEmpty() || !idName.getText().matches("^[a-zA-ZčćđžšČĆŽŠĐ]{2,20}$")) {
                     idName.getStyleClass().removeAll("rightTextField");
                     idName.getStyleClass().add("wrongTextField");
                 } else {
@@ -50,7 +52,7 @@ public class SignupController {
         idSurname.textProperty().addListener(new ChangeListener<String>() {
             @Override
             public void changed(ObservableValue<? extends String> observableValue, String o, String n) {
-                if (idSurname.getText().trim().isEmpty()) {
+                if (idSurname.getText().trim().isEmpty() || !idSurname.getText().matches("^[a-zA-ZčćđžšČĆŽŠĐd]{2,20}$")) {
                     idSurname.getStyleClass().removeAll("rightTextField");
                     idSurname.getStyleClass().add("wrongTextField");
                 } else {
@@ -62,7 +64,7 @@ public class SignupController {
         idEmail.textProperty().addListener(new ChangeListener<String>() {
             @Override
             public void changed(ObservableValue<? extends String> observableValue, String o, String n) {
-                if (idEmail.getText().trim().isEmpty()) {
+                if (idEmail.getText().trim().isEmpty() || !idEmail.getText().matches("^[\\w-]+(?:\\.[a-zA-Z0-9]+)*@[a-zA-Z0-9]+(?:\\.[a-zA-Z0-9]+)*$")) {
                     idEmail.getStyleClass().removeAll("rightTextField");
                     idEmail.getStyleClass().add("wrongTextField");
                 } else {
@@ -74,7 +76,7 @@ public class SignupController {
         idPassword.textProperty().addListener(new ChangeListener<String>() {
             @Override
             public void changed(ObservableValue<? extends String> observableValue, String o, String n) {
-                if (idPassword.getText().trim().isEmpty()) {
+                if (idPassword.getText().trim().isEmpty() || idPassword.getText().length()<8) {
                     idPassword.getStyleClass().removeAll("rightTextField");
                     idPassword.getStyleClass().add("wrongTextField");
                 } else {
@@ -86,7 +88,7 @@ public class SignupController {
         idAccountNumber.textProperty().addListener(new ChangeListener<String>() {
             @Override
             public void changed(ObservableValue<? extends String> observableValue, String o, String n) {
-                if (idAccountNumber.getText().trim().isEmpty()) {
+                if (idAccountNumber.getText().trim().isEmpty() || !idAccountNumber.getText().matches("^[0-9]{5,30}$")) {
                     idAccountNumber.getStyleClass().removeAll("rightTextField");
                     idAccountNumber.getStyleClass().add("wrongTextField");
                 } else {
@@ -104,11 +106,11 @@ public class SignupController {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/login.fxml"));
             Parent root = loader.load();
             Scene scene = new Scene(root,USE_COMPUTED_SIZE,USE_COMPUTED_SIZE);
-            stage.setTitle("Sing up");
+            stage.setTitle("Log in");
             stage.setScene(scene);
             stage.setResizable(false);
             stage.show();
-            Stage primaryStage = (Stage) idSignup.getScene().getWindow();
+            Stage primaryStage = (Stage) idLogin.getScene().getWindow();
             primaryStage.hide();
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -116,11 +118,27 @@ public class SignupController {
     }
     public void signupButtonAction(ActionEvent actionEvent) {
         boolean tacan_unos=true;
-        if(!idName.getText().matches("^[a-zA-ZčćđžšČĆŽŠĐ\\d]{2,20}$") || idName.getText().isEmpty()) tacan_unos=false;
-        if(!idSurname.getText().matches("^[a-zA-ZčćđžšČĆŽŠĐ\\d]{2,20}$") || idSurname.getText().isEmpty()) tacan_unos=false;
-        if(!idEmail.getText().matches("^[\\w-]+(?:\\.[a-zA-Z0-9]+)*@[a-zA-Z0-9]+(?:\\.[a-zA-Z0-9]+)*$") || idEmail.getText().isEmpty()) tacan_unos=false;
-        if(!idAccountNumber.getText().matches("^[0-9]{5,30}$") || idAccountNumber.getText().isEmpty()) tacan_unos=false;
-        if(idPassword.getText().length()<8) tacan_unos=false;
+        String greska="";
+        if(!idName.getText().matches("^[a-zA-ZčćđžšČĆŽŠĐ]{2,20}$") || idName.getText().isEmpty()){
+            tacan_unos=false;
+            greska=greska+"Name can only contain letters and has to be longer than one letter!\n";
+        }
+        if(!idSurname.getText().matches("^[a-zA-ZčćđžšČĆŽŠĐd]{2,20}$") || idSurname.getText().isEmpty()){
+            tacan_unos=false;
+            greska=greska+"Surname can only contain letters and has to be longer than one letter!\n";
+        }
+        if(!idEmail.getText().matches("^[\\w-]+(?:\\.[a-zA-Z0-9]+)*@[a-zA-Z0-9]+(?:\\.[a-zA-Z0-9]+)*$") || idEmail.getText().isEmpty()){
+            tacan_unos=false;
+            greska=greska+"Invalid email input! \n";
+        }
+        if(!idAccountNumber.getText().matches("^[0-9]{5,30}$") || idAccountNumber.getText().isEmpty()){
+            tacan_unos=false;
+            greska=greska+"Account Number can only contain numbers!\n";
+        }
+        if(idPassword.getText().length()<8){
+            tacan_unos=false;
+            greska=greska+"Password must be longer than 7! \n";
+        }
         if(tacan_unos){
             Buyer buyer=new Buyer();
             buyer.setName(idName.getText());
@@ -130,16 +148,19 @@ public class SignupController {
             buyer.setPassword(idPassword.getText());
             Random random=new Random();
             buyer.setAccount_balance(random.nextInt(5000));
+            BuyerDaoSQLImpl buyerDaoSQL=new BuyerDaoSQLImpl();
+            buyerDaoSQL.insert(buyer);
             try {
                 Stage stage = new Stage();
-                FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/search_phones.fxml"));
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/home.fxml"));
                 Parent root = loader.load();
                 Scene scene = new Scene(root,USE_COMPUTED_SIZE,USE_COMPUTED_SIZE);
-                stage.setTitle("Search");
+                stage.setTitle("Home page");
                 stage.setScene(scene);
                 stage.setResizable(false);
-                SearchPhonesController searchPhonesController=new SearchPhonesController(buyer);
-                loader.setController(searchPhonesController);
+                HomeController homeController=new HomeController();
+                homeController.setBuyer(buyer);
+                loader.setController(homeController);
                 stage.show();
                 Stage primaryStage = (Stage) idSignup.getScene().getWindow();
                 primaryStage.hide();
@@ -147,6 +168,13 @@ public class SignupController {
                 throw new RuntimeException(e);
             }
         }
-        else System.out.println("Netacno");
+        else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.getDialogPane().setPrefSize(400, 250);
+            alert.setTitle("Incorrect input");
+            alert.setHeaderText("Error");
+            alert.setContentText(greska);
+            alert.showAndWait();
+        }
     }
 }
