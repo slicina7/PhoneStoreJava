ff666b79fb448027c622c6874694e465a0a68b73
slicina7 <slicina1@etf.unsa.ba>
2022-12-29 22:48:25 +0100

    Add button action

--- src/main/java/ba/unsa/etf/rpr/controllers/EditPhonesController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/EditPhonesController.java
@@ -1,12 +1,16 @@
 package ba.unsa.etf.rpr.controllers;
 
 import ba.unsa.etf.rpr.dao.BrandDaoSQLImpl;
+import ba.unsa.etf.rpr.dao.PhoneDaoSQLImpl;
 import ba.unsa.etf.rpr.domain.Brand;
+import ba.unsa.etf.rpr.domain.Phone;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+
+import java.sql.Date;
 
 public class EditPhonesController {
     public ChoiceBox<Brand> brandChoiceBox;
@@ -16,10 +20,14 @@ public class EditPhonesController {
     public DatePicker releaseDate;
     public Spinner<Integer> priceSpinner;
     public Spinner<Integer> stockSpinner;
+    Phone phone;
+    PhoneDaoSQLImpl phoneDaoSQL;
 
     public EditPhonesController() {
         brandDaoSQL=new BrandDaoSQLImpl();
         brands= FXCollections.observableArrayList(brandDaoSQL.getAll());
+        phone=new Phone();
+        phoneDaoSQL=new PhoneDaoSQLImpl();
     }
 
     @FXML
@@ -31,6 +39,13 @@ public class EditPhonesController {
         stockSpinner.setValueFactory(stockFactory);
     }
     public void addButtonAction(ActionEvent actionEvent) {
+        phone.setVersion(versionTextField.getText());
+        phone.setBrand(brandChoiceBox.getValue());
+        phone.setPrice(priceSpinner.getValue());
+        phone.setIn_stock(stockSpinner.getValue());
+        phone.setRelease_date(Date.valueOf(releaseDate.getValue()));
+        System.out.println(phone);
+        phoneDaoSQL.insert(phone);
     }
 
     public void updateButtonAction(ActionEvent actionEvent) {
