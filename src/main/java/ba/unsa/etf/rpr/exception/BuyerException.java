16e5b68997dd01ceb1003ab3b9d0fd5ae42aa50f
slicina7 <slicina1@etf.unsa.ba>
2022-12-29 18:55:47 +0100

    Final version of exception

--- src/main/java/ba/unsa/etf/rpr/exception/BuyerException.java
+++ src/main/java/ba/unsa/etf/rpr/exception/BuyerException.java
@@ -1,8 +1,15 @@
 package ba.unsa.etf.rpr.exception;
-
+/**
+ * Exceptios for DAO classes
+ *
+ * @author Selma Ličina
+ */
 public class BuyerException extends Exception{
     public BuyerException(String message){
         super(message);
     }
 
+    public BuyerException(String message, Exception reason) {
+        super(message, reason);
+    }
 }
