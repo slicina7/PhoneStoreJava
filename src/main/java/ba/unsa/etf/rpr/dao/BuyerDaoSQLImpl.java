a062c81188772d39a21e895919610cbf22901aca
slicina7 <slicina1@etf.unsa.ba>
2023-01-02 21:30:46 +0100

    Refactoring

--- src/main/java/ba/unsa/etf/rpr/dao/BuyerDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/BuyerDaoSQLImpl.java
@@ -1,173 +1,47 @@
 package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Buyer;
-import ba.unsa.etf.rpr.domain.Purchase;
 import ba.unsa.etf.rpr.exception.BuyerException;
-
-import java.io.FileReader;
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Properties;
+import java.util.Map;
+import java.util.TreeMap;
 
-public class BuyerDaoSQLImpl implements BuyerDao{
+public class BuyerDaoSQLImpl extends AbstractDao<Buyer> implements BuyerDao{
 
-    private Connection connection;
+    public BuyerDaoSQLImpl(){super("buyers");}
 
-    public BuyerDaoSQLImpl(){
+    @Override
+    public Buyer rowToObject(ResultSet rs) throws BuyerException {
         try{
-            FileReader reader=new FileReader("db.properties");
-            Properties p=new Properties();
-            p.load(reader);
-            this.connection= DriverManager.getConnection(p.getProperty("url"),p.getProperty("user"),p.getProperty("password"));
-        }catch(Exception e){
-            e.printStackTrace();
+            Buyer buyer=new Buyer();
+            buyer.setId(rs.getInt("id"));
+            buyer.setName(rs.getString("name"));
+            buyer.setSurname(rs.getString("surname"));
+            buyer.setEmail(rs.getString("email"));
+            buyer.setAccount_number(rs.getString("account_number"));
+            buyer.setPassword(rs.getString("password"));
+            buyer.setAccount_balance(rs.getInt("account_balance"));
+            return buyer;
+        }catch(SQLException e){
+            throw new BuyerException(e.getMessage(),e);
         }
     }
 
     @Override
-    public Buyer getById(int id) {
-        String query = "SELECT * FROM buyers WHERE id = ?";
-        try{
-            PreparedStatement stmt=this.connection.prepareStatement(query);
-            stmt.setInt(1,id);
-            ResultSet rs=stmt.executeQuery();  // rs is iterator
-            if(rs.next()){
-                Buyer buyer=new Buyer();
-                buyer.setId(rs.getInt("id"));
-                buyer.setName(rs.getString("name"));
-                buyer.setSurname(rs.getString("surname"));
-                buyer.setEmail(rs.getString("email"));
-                buyer.setAccount_number(rs.getString("account_number"));
-                buyer.setPassword(rs.getString("password"));
-                buyer.setAccount_balance(rs.getInt("account_balance"));
-                rs.close();
-                return buyer;
-            }else
-                return null; // no elements in rs
-        }catch (SQLException e){
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    @Override
-    public Buyer insert(Buyer item) {
-        String insert = "INSERT INTO buyers VALUES(?,?,?,?,?,?)";
-        try{
-            PreparedStatement stmt=this.connection.prepareStatement(insert);
-            stmt.setInt(1,item.getId());
-            stmt.setString(2, item.getName());
-            stmt.setString(3,item.getSurname());
-            stmt.setString(4, item.getEmail());
-            stmt.setString(5,item.getAccount_number());
-            stmt.setString(6,item.getPassword());
-            stmt.setInt(7,item.getAccount_balance());
-            stmt.executeUpdate();
-            return item;
-        }catch (SQLException e){
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    @Override
-    public Buyer update(Buyer item) {
-        String insert = "UPDATE buyers SET account_balance = ? WHERE id = ?";
-        try{
-            PreparedStatement stmt = this.connection.prepareStatement(insert);
-
-            stmt.setInt(1,item.getAccount_balance());
-            stmt.setInt(2, item.getId());
-            stmt.executeUpdate();
-            return item;
-        }catch (SQLException e){
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    @Override
-    public void delete(int id) {
-        String insert = "DELETE FROM buyers WHERE id = ?";
-        try{
-            PreparedStatement stmt = this.connection.prepareStatement(insert);
-            stmt.setInt(1, id);
-            stmt.executeUpdate();
-        }catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public List<Buyer> getAll() {
-        String query = "SELECT * FROM buyers";
-        List<Buyer> buyers = new ArrayList<Buyer>();
-        try{
-            PreparedStatement stmt = this.connection.prepareStatement(query);
-            ResultSet rs = stmt.executeQuery();
-            while (rs.next()){ // result set is iterator.
-                Buyer buyer=new Buyer();
-                buyer.setId(rs.getInt("id"));
-                buyer.setName(rs.getString("name"));
-                buyer.setSurname(rs.getString("surname"));
-                buyer.setEmail(rs.getString("email"));
-                buyer.setAccount_number(rs.getString("account_number"));
-                buyer.setPassword(rs.getString("password"));
-                buyer.setAccount_balance(rs.getInt("account_balance"));
-                buyers.add(buyer);
-            }
-            rs.close();
-        }catch (SQLException e){
-            e.printStackTrace(); // poor error handling
-        }
-        return buyers;
-    }
-    @Override
-    public void tableView() {
-        String query="SELECT * FROM buyers";
-        try {
-            PreparedStatement stmt = this.connection.prepareStatement(query);
-            ResultSet rs = stmt.executeQuery();
-            while(rs.next()){
-                int id=rs.getInt("id");
-                String name=rs.getString("name");
-                String surname=rs.getString("surname");
-                String email=rs.getString("email");
-                String account_number=rs.getString("account_number");
-                String password=rs.getString("password");
-                int balance=rs.getInt("account_balance");
-                System.out.println(id+" "+name+" "+surname+" "+email+" "+account_number+" "+password+" "+balance);
-            }
-        }catch (Exception e){
-            e.printStackTrace();
-        }
+    public Map<String, Object> objectToRow(Buyer object) {
+        Map<String,Object> item=new TreeMap<>();
+        item.put("id",object.getId());
+        item.put("name",object.getName());
+        item.put("surname",object.getSurname());
+        item.put("email",object.getEmail());
+        item.put("account_number",object.getAccount_number());
+        item.put("password",object.getPassword());
+        item.put("account_balance",object.getAccount_balance());
+        return item;
     }
 
     @Override
     public Buyer searchByEmailAndPassword(String email,String password) throws BuyerException {
-        String query = "SELECT * FROM buyers WHERE email = ? AND password = ?";
-        Buyer buyer=new Buyer();
-        try{
-            PreparedStatement stmt = this.connection.prepareStatement(query);
-            stmt.setString(1,email);
-            stmt.setString(2,password);
-            ResultSet rs = stmt.executeQuery();
-            if (rs.next()){
-                buyer.setId(rs.getInt("id"));
-                buyer.setName(rs.getString("name"));
-                buyer.setSurname(rs.getString("surname"));
-                buyer.setEmail(rs.getString("email"));
-                buyer.setAccount_number(rs.getString("account_number"));
-                buyer.setPassword(rs.getString("password"));
-                buyer.setAccount_balance(rs.getInt("account_balance"));
-            }
-            rs.close();
-            if(buyer.getName()==null || buyer.getSurname()==null) throw new BuyerException(("Account doesn't exist!"));
-            return buyer;
-        }catch (SQLException e){
-            e.printStackTrace(); // poor error handling
-        }
-        return buyer;
+        return executeQueryUnique("\"SELECT * FROM buyers WHERE email = ? AND password = ?",new Object[]{email,password});
     }
 }
