7e4a2eac8c716f2c0ff98bfc4f5e6833dead1bee
slicina7 <slicina1@etf.unsa.ba>
2023-01-13 16:27:03 +0100

    Brand manager

---
+++ src/main/java/ba/unsa/etf/rpr/business/BrandManager.java
@@ -0,0 +1,30 @@
+package ba.unsa.etf.rpr.business;
+
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.Brand;
+import ba.unsa.etf.rpr.domain.Buyer;
+import ba.unsa.etf.rpr.exception.BuyerException;
+
+import java.util.List;
+
+public class BrandManager {
+    public List<Brand> getAll() throws BuyerException {
+        return DaoFactory.brandDao().getAll();
+    }
+
+    public void delete(int id) throws BuyerException{
+        DaoFactory.brandDao().delete(id);
+    }
+    public Brand getById(int id) throws BuyerException{
+        return DaoFactory.brandDao().getById(id);
+    }
+    public void update(Brand brand) throws  BuyerException{
+        DaoFactory.brandDao().update(brand);
+    }
+    public void insert(Brand brand) throws BuyerException{
+        DaoFactory.brandDao().insert(brand);
+    }
+    public Brand searchByName(String name) throws BuyerException{
+        return DaoFactory.brandDao().searchByName(name);
+    }
+}
