d5420fbb000c90f3b98fc3fd13cfe7f729a298c5
slicina7 <slicina1@etf.unsa.ba>
2023-01-13 17:05:57 +0100

    Final version

--- src/main/java/ba/unsa/etf/rpr/controllers/EditBrandsController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/EditBrandsController.java
@@ -1,20 +1,32 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.business.BrandManager;
 import ba.unsa.etf.rpr.dao.BrandDaoSQLImpl;
 import ba.unsa.etf.rpr.domain.Brand;
+import ba.unsa.etf.rpr.exception.BuyerException;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 
 public class EditBrandsController {
     public TextField brandTextField;
+    public BrandManager brandManager=new BrandManager();
 
-    public void addButtonAction(ActionEvent actionEvent) {
-        Brand brand=new Brand();
-        brand.setName(brandTextField.getText());
-        BrandDaoSQLImpl brandDaoSQL=new BrandDaoSQLImpl();
-        brandDaoSQL.insert(brand);
+    public void addButtonAction(ActionEvent actionEvent)  {
+        try {
+            if(brandManager.searchByName(brandTextField.getText()).size()!=0)
+                new Alert(Alert.AlertType.ERROR,"That brand already exists!");
+            else{
+                Brand brand=new Brand();
+                brand.setName(brandTextField.getText());
+                brandManager.insert(brand);
+            }
+        }catch(BuyerException e){
+
+        }
+
     }
-
     public void cancelButtonAction(ActionEvent actionEvent) {
+        brandTextField.getScene().getWindow().hide();
     }
 }
