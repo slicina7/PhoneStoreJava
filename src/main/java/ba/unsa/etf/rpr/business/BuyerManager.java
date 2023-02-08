bfa76f5c0ab062dde99279d666919515701b7d0b
slicina7 <slicina1@etf.unsa.ba>
2023-02-08 23:49:53 +0100

    Adding validator for name,surname and email with regex

--- src/main/java/ba/unsa/etf/rpr/business/BuyerManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/BuyerManager.java
@@ -8,6 +8,18 @@ import ba.unsa.etf.rpr.exception.BuyerException;
 import java.util.List;
 
 public class BuyerManager {
+    public void validateName(String name) throws BuyerException{
+        if(name==null || name.length()>30 || !name.matches("^[a-zA-Z]{2,20}$") || name.isEmpty())
+            throw new BuyerException("Invalid name");
+    }
+    public void validateSurname(String surname) throws BuyerException{
+        if(surname==null || surname.length()>30 || !surname.matches("^[a-zA-Z]{2,20}$") || surname.isEmpty())
+            throw new BuyerException("Invalid surname");
+    }
+    public void validateEmail(String email) throws BuyerException{
+        if(email==null || email.length()>30 || !email.matches("^[\\w-]+(?:\\.[a-zA-Z0-9]+)*@[a-zA-Z0-9]+(?:\\.[a-zA-Z0-9]+)*$") || email.isEmpty())
+            throw new BuyerException("Invalid email");
+    }
     public List<Buyer> getAll() throws BuyerException{
         return DaoFactory.buyerDao().getAll();
     }
