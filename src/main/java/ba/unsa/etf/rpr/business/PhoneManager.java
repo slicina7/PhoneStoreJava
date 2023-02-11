2502a4debbaa8f203ddbf94d6f70ca9a491bf4f5
slicina7 <slicina1@etf.unsa.ba>
2023-02-11 19:40:30 +0100

    Added phone version validator

--- src/main/java/ba/unsa/etf/rpr/business/PhoneManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/PhoneManager.java
@@ -1,14 +1,19 @@
 package ba.unsa.etf.rpr.business;
 
-import ba.unsa.etf.rpr.dao.Dao;
 import ba.unsa.etf.rpr.dao.DaoFactory;
 import ba.unsa.etf.rpr.domain.Brand;
 import ba.unsa.etf.rpr.domain.Phone;
 import ba.unsa.etf.rpr.exception.BuyerException;
 
 import java.util.List;
+import java.util.Objects;
 
 public class PhoneManager {
+
+    public void validateVersion(String version) throws BuyerException{
+        if(version==null || version.length()<2 || version.length()>30 || !version.matches("^[a-zA-Z0-9]{2,20}$") || version.isEmpty())
+            throw new BuyerException("Phone version can only contain letters and numbers and has to be longer than two and shorter than 30 letters and numbers!");
+    }
     public List<Phone> getAll() throws BuyerException{
         return DaoFactory.phoneDao().getAll();
     }
@@ -28,9 +33,14 @@ public class PhoneManager {
         return DaoFactory.phoneDao().getById(id);
     }
     public void update(Phone phone) throws  BuyerException{
+        validateVersion(phone.getVersion());
         DaoFactory.phoneDao().update(phone);
     }
     public void insert(Phone phone) throws BuyerException{
+        validateVersion(phone.getVersion());
+        for(Phone p: getAll())
+            if(Objects.equals(p.getVersion(),phone.getVersion()))
+                throw new BuyerException("Phone with that version already exists !");
         DaoFactory.phoneDao().insert(phone);
     }
 
