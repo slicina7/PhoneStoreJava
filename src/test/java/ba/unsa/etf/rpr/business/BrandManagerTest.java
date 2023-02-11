a58ec345dbc8fe08a971c90af2fb1346f8c3bd0c
slicina7 <slicina1@etf.unsa.ba>
2023-02-11 19:57:19 +0100

    Added test for brand name validator

--- src/test/java/ba/unsa/etf/rpr/business/BrandManagerTest.java
+++ src/test/java/ba/unsa/etf/rpr/business/BrandManagerTest.java
@@ -2,6 +2,7 @@ package ba.unsa.etf.rpr.business;
 import ba.unsa.etf.rpr.dao.BrandDaoSQLImpl;
 import ba.unsa.etf.rpr.domain.Brand;
 import ba.unsa.etf.rpr.exception.BuyerException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -25,6 +26,22 @@ public class BrandManagerTest {
         brandDaoSQLMock=Mockito.mock(BrandDaoSQLImpl.class);
         brands=new ArrayList<>();
         brands.addAll(Arrays.asList(new Brand("Name2"),new Brand("Name3")));
+    }
+    void validateName() throws BuyerException{
+        String correctName="Name";
+        try{
+            Mockito.doCallRealMethod().when(brandManager).validateName(correctName);
+        }catch(BuyerException e){
+            e.printStackTrace();
+            Assertions.assertTrue(false);
+        }
+
+        String incorrectShort="S";
+        Mockito.doCallRealMethod().when(brandManager).validateName(incorrectShort);
+        BuyerException buyerException1=assertThrows(BuyerException.class,()->{
+            brandManager.validateName(incorrectShort);},"Brand name can only contain letters and numbers and has to be longer than two and shorter than 20 letters and numbers!") ;
+        Assertions.assertEquals("Brand name can only contain letters and numbers and has to be longer than two and shorter than 20 letters and numbers!",buyerException1.getMessage());
+
     }
     @Test
     void insertException() {
