0c725f634b07e63899eff2b3937ee54d77564bfe
slicina7 <slicina1@etf.unsa.ba>
2023-02-15 20:02:32 +0100

    Added test for searching by brand and version

--- src/test/java/ba/unsa/etf/rpr/business/PhoneManagerTest.java
+++ src/test/java/ba/unsa/etf/rpr/business/PhoneManagerTest.java
@@ -18,49 +18,30 @@ class PhoneManagerTest {
     @Test
     void uniqueVersion() throws BuyerException {
         Brand b=brandManager.searchByName("Huawei");
-        Phone phone=new Phone(b,"Unique version",100,10,new Date(2000,3,5));
+        Phone phone=new Phone(b,"Unique version",100,10,new Date(100,3,5));
         phoneManager.insert(phone);
         assertThrows(BuyerException.class, ()->
-                phoneManager.insert(new Phone(b,"Unique version",200,20,new Date(2020,10,15))));
+                phoneManager.insert(new Phone(b,"Unique version",200,20,new Date(120,10,15))));
         phoneManager.delete(phoneManager.searchByBrandAndVersion(b,"Unique version").getId());
     }
     @Test
     void validateVersion() {
         String shortNameForVersion="P";
-       assertThrows(BuyerException.class,()->phoneManager.insert(new Phone(brandManager.searchByName("Samsung"),shortNameForVersion,1000,10,new Date(2010,10,10))));
+       assertThrows(BuyerException.class,()->phoneManager.insert(new Phone(brandManager.searchByName("Samsung"),shortNameForVersion,1000,10,new Date(120,10,10))));
     }
 
     @Test
-    void getAll() {
-    }
-
-    @Test
-    void searchByBrand() {
-    }
-
-    @Test
-    void searchByPrice() {
-    }
-
-    @Test
-    void searchByBrandAndVersion() {
-    }
-
-    @Test
-    void delete() {
-    }
-
-    @Test
-    void getById() {
-    }
-
-    @Test
-    void update() {
+    void searchByBrandAndVersion() throws BuyerException{
+        Phone p=new Phone(brandManager.searchByName("Samsung"),"Version",1000,20,new Date(120,10,10));
+        phoneManager.insert(p);
+        Phone p2=phoneManager.searchByBrandAndVersion(brandManager.searchByName("Samsung"),"Version");
+        assertEquals(p,p2);
+        phoneManager.delete(p2.getId());
     }
 
     @Test
     void insert() throws BuyerException{
-        Phone p=new Phone(brandManager.searchByName("Apple"),"Version",1000,20,new Date(2020,10,10));
+        Phone p=new Phone(brandManager.searchByName("Apple"),"Version",1000,20,new Date(120,10,10));
         phoneManager.insert(p);
         Phone p2=phoneManager.searchByBrandAndVersion(brandManager.searchByName("Apple"),"Version");
         assertTrue(p2!=null);
