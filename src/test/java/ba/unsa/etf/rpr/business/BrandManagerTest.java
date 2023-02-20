390e3bffc78c5ab7b4b768b94de165b3a8b37d53
slicina7 <slicina1@etf.unsa.ba>
2023-02-20 17:00:10 +0100

    Minor changes

--- src/test/java/ba/unsa/etf/rpr/business/BrandManagerTest.java
+++ src/test/java/ba/unsa/etf/rpr/business/BrandManagerTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
